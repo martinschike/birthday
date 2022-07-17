a16f2f6bef5b277abb3dff0859ce02acc6a381fd
MARTINS OKAFOR <okaformartinschinwike@gmail.com>
2022-07-17 11:12:15 +0100

    first commit

---
+++ src/data.js
@@ -0,0 +1,36 @@
+export default [
+    {
+        id: 1,
+        name: "Martins Okafor",
+        age: 34,
+        image: "https://scontent.fbhx1-1.fna.fbcdn.net/v/t39.30808-6/286496591_10227669168344212_1320737560347712187_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=mdtdKqTc9SwAX-5coQP&_nc_ht=scontent.fbhx1-1.fna&oh=00_AT9jBo-mQC4AriYJB_oUhJYt17xvbfSydxIzYx2AKMb5Jw&oe=62D3770D",
+    },
+
+    {
+        id: 2,
+        name: "Chinelo Okafor",
+        age: 33,
+        image: "https://scontent.fbhx1-1.fna.fbcdn.net/v/t39.30808-6/289561622_5482056088518669_766687586057709702_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=USBdRaEDRSkAX_jAjf-&_nc_ht=scontent.fbhx1-1.fna&oh=00_AT9FEGpxAzOALoKa_rtDsxb5pzg1GZqOkVAeac3NZ9X1EA&oe=62D497CF",
+    },
+
+    {
+        id: 3,
+        name: "Emmanuel Okafor",
+        age: 32,
+        image: "https://scontent.fbhx1-1.fna.fbcdn.net/v/t39.30808-6/251440224_2957564767852995_832455735136462228_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=FYY9MC4og8gAX_kIXA5&_nc_ht=scontent.fbhx1-1.fna&oh=00_AT-aOY-01pL7HC5uMQJ4juhjFczzakAcxvk_6eIbIxh1Mw&oe=62D2F9F3",
+    },
+
+    {
+        id: 4,
+        name: "Nkiru Chibueze",
+        age: 28,
+        image: "https://scontent.fbhx1-1.fna.fbcdn.net/v/t39.30808-6/277774322_3077473609181072_7739105672845521476_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=M7X1BlweNUsAX-pJGPX&_nc_ht=scontent.fbhx1-1.fna&oh=00_AT9ARoIslObL6TYr1jPQpLL8_kdyUxz0t1DYbMpY2YdONg&oe=62D3D714",
+    },
+
+    {
+        id: 5,
+        name: "Chinenye Chibueze",
+        age: 22,
+        image: "https://scontent.fbhx1-1.fna.fbcdn.net/v/t1.6435-9/120298555_814518029304209_7659824081571497355_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=C2HSDrl4NLgAX_ExSif&_nc_oc=AQmwbNwGSHP2aUSuum4IW2vrVFOLa-jE83L5V5qtEJZyVUjBxHTHNEXsH1GFYllruKg&_nc_ht=scontent.fbhx1-1.fna&oh=00_AT_yLgHeKDr456Queg1W5tXVrOZ261c2qoINaj9v2Di9EQ&oe=62F30BCB",
+    },
+];
